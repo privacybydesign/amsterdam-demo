6dbfbe45d133d6f641ed585fe84ab3ebd16ef6a2
Edwin Martin <edwin@bitstorm.org>
2019-07-04 11:25:18 +0200

    Move dev files to seperate directory

--- dev/start.sh
+++ dev/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cd server
+cd ../voting_node/server
 STAGE=dev POSTGRES_HOST=localhost POSTGRES_DATABASE=postgres POSTGRES_USER=postgres POSTGRES_PASSWORD=hj21kjy npx nodemon index.js
