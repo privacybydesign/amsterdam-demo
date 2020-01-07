dba19cd69a91c8c98dbdbd65ecce00cab1cd3f30
Adrian Tudorache <a.p.tudorache@gmail.com>
2020-01-07 11:37:51 +0100

    replaces the image background with a link to the internet image

--- voting_node/di-demo/src/App.tsx
+++ voting_node/di-demo/src/App.tsx
@@ -7,22 +7,20 @@ const MainContainerStyle = styled.div`
   background-color: #F2F5F8;
 `;
 
-
-
 const AppWrapper = styled.div`
   postion: relative;
   text-align: center;
-  background-image: url(assets/Home.png);
-  background-repeat: no-repeat;
-  background-size: 1440px;
-  height: 100vh;
+  // background-image: url(assets/Home.png);
+  // background-repeat: no-repeat;
+  // background-size: 1440px;
+  min-height: 100vh;
   margin: 0 auto;
-  width: 1440px;
+  // width: 1440px;
 `;
 
 const LinkListStyle = styled.div`
   position: absolute;
-  top: 335px;
+  top: 326px;
   left: 0;
   right: 0;
   margin: auto;
@@ -32,10 +30,10 @@ const LinkListStyle = styled.div`
 `;
 
 const LinkStyle = styled.a`
-  /*border: solid 1px grey;*/
+  // border: solid 1px grey;
   width: 314px;
-  margin-bottom: 21px;
-  height: 54px;
+  margin-bottom: 20px;
+  height: 53px;
 `;
 
 const Spacer = styled.div<{ height: number }>`
@@ -46,13 +44,14 @@ const App: React.FC = () => {
   return (<MainContainerStyle>
 
     <AppWrapper>
+        <img alt="Home" src="https://s3.invisionapp-cdn.com/storage.invisionapp.com/screens/files/398337770.png?x-amz-meta-iv=3&amp;response-cache-control=max-age%3D2419200&amp;x-amz-meta-ck=6c5383e9abd1130f655ddd13ea0bf466&amp;AWSAccessKeyId=AKIAJFUMDU3L6GTLUDYA&amp;Expires=1580515200&amp;Signature=TZOGJH%2FKxtyL4QWifGzDYYfnTy0%3D" height="1068" width="1400" decoding="async"/>
         <LinkListStyle>
           <LinkStyle href="mijnamsterdam">{/* Mijn Amsterdam */}</LinkStyle>
           <LinkStyle href="openstad">{/* Open stad */}</LinkStyle>
           <LinkStyle href="halloijburg">{/* Hallo IJburg */}</LinkStyle>
           <LinkStyle href="alchoolkopen">{/* Alchook kopen */}</LinkStyle>
           <LinkStyle href="rommelmelden">{/* Rommel melden */}</LinkStyle>
-          <Spacer height={42} />
+          <Spacer height={41} />
           <LinkStyle href="kortingkrijgen">{/* Korting krijgen */}</LinkStyle>
         </LinkListStyle>
     </AppWrapper>
