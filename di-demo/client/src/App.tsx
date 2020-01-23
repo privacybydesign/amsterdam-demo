3c7273dd951749dc54ee1203c28d8e102cd827a3
Adrian Tudorache <a.p.tudorache@gmail.com>
2020-01-23 08:20:25 +0100

    implements the redesign of the home page

--- di-demo/client/src/App.tsx
+++ di-demo/client/src/App.tsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useContext } from "react";
-import styled from '@datapunt/asc-core';
+import styled from "@datapunt/asc-core";
+import { useHistory } from "react-router-dom";
 import "./App.css";
 import { PageWrapper } from "./AppStyle";
+import Button, {
+  ButtonStyleProps
+} from "./shared/components/Button/ButtonStyle";
 
 const MainContainerStyle = styled.div`
   background-color: #f2f5f8;
@@ -29,8 +33,34 @@ const Spacer = styled.div<{ height: number }>`
   height: ${({ height }) => height}px;
 `;
 
+const mijnAmsterdamButtonPosition: ButtonStyleProps = {
+  width: 314,
+  height: 53,
+  top: 326,
+  left: 543
+};
+const openStadButtonPosition: ButtonStyleProps = {
+  width: 314,
+  height: 53,
+  top: 401,
+  left: 543
+};
+const afvalMeldenButtonPosition: ButtonStyleProps = {
+  width: 314,
+  height: 53,
+  top: 476,
+  left: 543
+};
+const alchoolButtonPosition: ButtonStyleProps = {
+  width: 314,
+  height: 53,
+  top: 551,
+  left: 543
+};
+
 const themeName = "amsterdam";
 const App: React.FC = () => {
+  const history = useHistory();
 
   return (
     <MainContainerStyle>
@@ -42,27 +72,22 @@ const App: React.FC = () => {
           width="1400"
           decoding="async"
         />
-        <LinkListStyle>
-          <LinkStyle href={`mijnamsterdam/${themeName}`}>
-            {/* Mijn Amsterdam */}
-          </LinkStyle>
-          <LinkStyle href={`openstad/${themeName}`}>
-            {/* Open stad */}
-          </LinkStyle>
-          <LinkStyle href={`halloijburg/${themeName}`}>
-            {/* Hallo IJburg */}
-          </LinkStyle>
-          <LinkStyle href={`alchoolkopen/${themeName}`}>
-            {/* Alchool kopen */}
-          </LinkStyle>
-          <LinkStyle href={`rommelmelden/${themeName}`}>
-            {/* Rommel melden */}
-          </LinkStyle>
-          <Spacer height={41} />
-          <LinkStyle href={`kortingkrijgen/${themeName}`}>
-            {/* Korting krijgen */}
-          </LinkStyle>
-        </LinkListStyle>
+        <Button
+          onClick={() => history.push(`mijnamsterdam/${themeName}`)}
+          {...mijnAmsterdamButtonPosition}
+        ></Button>
+        <Button
+          onClick={() => history.push(`openstad/${themeName}`)}
+          {...openStadButtonPosition}
+        ></Button>
+        <Button
+          onClick={() => history.push(`afvalmelden/${themeName}`)}
+          {...afvalMeldenButtonPosition}
+        ></Button>
+        <Button
+          onClick={() => history.push(`alchoolkopen/${themeName}`)}
+          {...alchoolButtonPosition}
+        ></Button>
       </PageWrapper>
     </MainContainerStyle>
   );
--- di-demo/client/src/AppRoutes.ts
+++ di-demo/client/src/AppRoutes.ts
@@ -1,10 +1,8 @@
 import App from "./App";
 import MijnAmsterdamPage from "./pages/MijnAmsterdamPage/MijnAmsterdamPage";
 import OpenStadPage from "./pages/OpenStadPage/OpenStadPage";
-import HalloIJburgPage from "./pages/HalloIJburgPage";
 import AlchoolKopenPage from "./pages/AlchoolKopenPage";
 import RommelMeldenPage from "./pages/RommelMeldenPage";
-import KortingKrijgenPage from "./pages/KortingKrijgenPage";
 
 const AppRoutes: any = {
     HOME: { path: "/", component: App },
@@ -13,13 +11,8 @@ const AppRoutes: any = {
       component: MijnAmsterdamPage
     },
     OPEN_STAD: { path: "/openstad/:theme", component: OpenStadPage },
-    HALLO_IJBURG: { path: "/halloijburg/:theme", component: HalloIJburgPage },
+    AFVAL_MELDEN: { path: "/afvalmelden/:theme", component: RommelMeldenPage },
     ALCHOOL_KOPEN: { path: "/alchoolkopen/:theme", component: AlchoolKopenPage },
-    ROMMEL_MELDEN: { path: "/rommelmelden/:theme", component: RommelMeldenPage },
-    KORTING_KRIJGEN: {
-      path: "/kortingkrijgen/:theme",
-      component: KortingKrijgenPage
-    }
   };
   
   export default AppRoutes;
