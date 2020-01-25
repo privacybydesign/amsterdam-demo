4cada63d2e5520b562b95806960208de9624ad4c
Adrian Tudorache <a.p.tudorache@gmail.com>
2020-01-25 14:12:56 +0100

    fixes jest errors

--- di-demo/client/src/App.test.tsx
+++ di-demo/client/src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders without errors', () => {
   const { getByTestId } = render(<App />);
   expect(getByTestId('main-container')).toBeInTheDocument();
 });
