37bdc87f81fd695fd4c441a0a7ebf9c1267e4f3a
Jasper Swart <jasper.g.swart@gmail.com>
2020-06-25 11:50:28 +0200

    added Article to Homepage

--- di-demo/client/src/components/Homepage/Homepage.tsx
+++ di-demo/client/src/components/Homepage/Homepage.tsx
@@ -1,19 +1,49 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { Button, Heading, Row, Column, themeSpacing, themeColor } from '@datapunt/asc-ui';
-
-const H1 = styled(Heading)`
-    margin-bottom: ${themeSpacing(25)};
-    background-color: ${themeColor('tint', 'level4')};
+import { Heading, Row, Column, Paragraph } from '@datapunt/asc-ui';
+import Article from '../../components/Article';
+const StyledImage = styled.img`
+    width: 100%;
+    margin-bottom: 12px;
 `;
 
+const StyledH1 = styled(Heading)`
+    margin-top: 16px;
+    margin-bottom: 24px;
+`;
+
+const StyledH2 = styled(Heading)`
+    margin-bottom: 12px;
+`;
 const Homepage: React.FC<unknown> = () => (
     <>
         <Row hasMaxWidth={false}>
             <Column span={{ small: 12, medium: 6, big: 6, large: 6, xLarge: 6 }}>
-                <H1>Homepage</H1>
-                <Button color="primary">Click me!</Button>
+                <div>
+                    <StyledH1>Probeer IRMA uit</StyledH1>
+
+                    <StyledImage src="/assets/home.png"></StyledImage>
+
+                    <Paragraph strong>
+                        IRMA is een app waarmee je overal kunt aantonen wie u bent. IRMA biedt een nieuwe manier van
+                        inloggen anders dan u kent van misschien DigiD.
+                    </Paragraph>
+
+                    <Paragraph strong>Binnenkort kunt u IRMA gebruiken in Amsterdam. Waarom IRMA?</Paragraph>
+
+                    <Paragraph strong>Ervaar nu vast wat u met IRMA kunt via verschillende demo's.</Paragraph>
+
+                    <StyledH2>Aan de slag met IRMA</StyledH2>
+
+                    {/* <Article imageSrc="/assets/demo_1.png" title="Demo 1: Leeftijd aantonen" href="/">
+                        Bewijs dat u ouder bent dan 18 jaar zonder uw geboortedatum prijs te geven.
+                    </Article>
+
+                    <Article imageSrc="/assets/demo_2.png" title="Demo 2: Ideeën voor uw buurt" href="/">
+                        Bewijs dat u in een bepaalde Amsterdamse wijk woont en dat u ouder bent dan 18 jaar.
+                    </Article> */}
+                </div>
             </Column>
         </Row>
     </>
