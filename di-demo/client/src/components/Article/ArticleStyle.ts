2f91ce31b368cbb59227f8b6ea5d1e590119e99c
Tom Jonkman <tom@tomjonkman.com>
2020-06-29 15:07:50 +0200

    Fix linting issues

--- di-demo/client/src/components/Article/ArticleStyle.ts
+++ di-demo/client/src/components/Article/ArticleStyle.ts
@@ -4,34 +4,34 @@ import styled from 'styled-components';
 import Article from './Article';
 
 const ArticleStyle = styled(Article)`
-   border-right: 2px solid black;
-   padding-bottom: 16px;
-   margin-bottom: 28px;
+    border-right: 2px solid black;
+    padding-bottom: 16px;
+    margin-bottom: 28px;
 
-   a {
-     color: ${themeColor('tint', 'level7')};
-     text-decoration: none;
-     display: flex
-         flex-direction: column;
+    a {
+        color: ${themeColor('tint', 'level7')};
+        text-decoration: none;
+        display: flex;
+        flex-direction: column;
 
-     h2 {
-       margin-bottom: ${themeSpacing(5)};
-     }
+        h2 {
+            margin-bottom: ${themeSpacing(5)};
+        }
 
-     img {
-       width: 100%;
-       margin-bottom: ${themeSpacing(4)};
-     }
+        img {
+            width: 100%;
+            margin-bottom: ${themeSpacing(4)};
+        }
 
-     div {
-       margin-bottom: ${themeSpacing(3)};
-     }
+        div {
+            margin-bottom: ${themeSpacing(3)};
+        }
 
-     span {
-       margin-right: ${themeSpacing(3)};
-       align-self: flex-end;
-     }
-   }
- `;
+        span {
+            margin-right: ${themeSpacing(3)};
+            align-self: flex-end;
+        }
+    }
+`;
 
 export default ArticleStyle;
