da82008c433305565a9d003b47d4ec2a20daa173
Tom Jonkman <tom@tomjonkman.com>
2020-07-02 12:23:59 +0200

    Add types and fix linting issues

--- di-demo/client/src/services/createIrmaSession/createIrmaSession.ts
+++ di-demo/client/src/services/createIrmaSession/createIrmaSession.ts
@@ -1,24 +1,34 @@
 import axios from 'axios';
 import * as irma from '@privacybydesign/irmajs';
 
+// Types
+interface IIrmaServerConfig {
+    requestorname: string;
+    uuid: string;
+    irma: string;
+    nodeUrl: string;
+    docroot: string;
+    port: number;
+}
+
 const port = 8000;
 
 const instance = axios.create({
     baseURL: `${window.location.protocol}//${window.location.hostname}:${port}`
 });
 
-let config: any;
+let config: IIrmaServerConfig;
 
-export const getConfig = async () => {
+export const getConfig = async (): Promise<IIrmaServerConfig> => {
     const response = await instance.get('/config');
     return response.data;
 };
 
-export const isMobile = () => {
+export const isMobile = (): boolean => {
     return /Android/i.test(window.navigator.userAgent) || /iPhone/.test(window.navigator.userAgent);
 };
 
-const createIrmaSession = async (dataType: string, holderElementId: string) => {
+const createIrmaSession = async (dataType: string, holderElementId: string): Promise<unknown> => {
     if (!config) {
         config = await getConfig();
     }
@@ -44,7 +54,7 @@ const createIrmaSession = async (dataType: string, holderElementId: string) => {
 
     const result = await irma.handleSession(sessionPtr, sessionOptions);
     const data = result.disclosed[0].reduce((acc, { id, rawvalue }) => ({ ...acc, [id]: rawvalue }), {});
-    console.log('data', data);
+    // console.log('data', data);
     return data;
 };
 
