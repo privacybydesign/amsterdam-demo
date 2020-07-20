277b12930368216c99a8d2301cc9a0a202efd44f
Jasper Swart <jasper.g.swart@gmail.com>
2020-07-20 15:40:12 +0200

    fixed result in demo-1 comp

--- client/src/components/Demo-1/Demo-1.tsx
+++ client/src/components/Demo-1/Demo-1.tsx
@@ -17,10 +17,12 @@ const StyledH1 = styled(Heading)`
 
 const Demo1: React.FC<IProps> = () => {
   const [isOver18, setIsOver18] = useState<boolean>(false);
+  const [hasResult, setHasResult] = useState<boolean>(false);
+
   const getSession = async () => {
     const response = await createIrmaSession('age', 'irma-qr');
     setIsOver18(response['pbdf.gemeente.personalData.over18'] === 'Yes');
-
+    setHasResult(true);
   }
 
   return (
@@ -29,15 +31,22 @@ const Demo1: React.FC<IProps> = () => {
         <BreadCrumbs.Item href="/">Home</BreadCrumbs.Item>
       </BreadCrumbs>
 
-      <div>
-        <StyledH1>Demo 1</StyledH1>
+      <StyledH1>Demo 1: uw leeftijd</StyledH1>
 
-        <Paragraph>Bent u ouder an 18?</Paragraph>
+      {!hasResult ?
+        <div>
 
-        <StyledH1>{isOver18 === true ? 'JA' : 'nee'}</StyledH1>
+          <Paragraph>Bent u ouder an 18?</Paragraph>
 
-        <QRCode getSession={getSession} />
-      </div>
+          <QRCode getSession={getSession} />
+        </div>
+
+        :
+        <div>
+          <StyledH1>{isOver18 === true ? 'JA' : 'nee'}</StyledH1>
+        </div>
+      }
+
     </PageTemplate>
   );
 };
