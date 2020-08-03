11d018e6d805aff496a85a2377a70e269e7878c2
Jasper Swart <jasper.g.swart@gmail.com>
2020-08-03 14:16:42 +0200

    refactored to getGGW

--- client/src/services/getGGW.ts
+++ client/src/services/getGGW.ts
@@ -11,9 +11,9 @@ const getGGW = async (postcode: string): Promise<IProps> => {
   const response = await instance.get(`https://api.data.amsterdam.nl/dataselectie/bag/?size=1&postcode=${postcode}`);
   if (response.data.aggs_list.ggw_naam) {
     const ggwResponse = response.data.aggs_list.ggw_naam;
-    console.log('WIJK rsponse', ggwResponse);
-    console.log('aantal wijken in postcode', ggwResponse.doc_count);
-    console.log('array met alle wijken in de postcode', ggwResponse.buckets);
+    console.log('GGW rsponse', ggwResponse);
+    console.log('aantal ggws in postcode', ggwResponse.doc_count);
+    console.log('array met alle ggws in de postcode', ggwResponse.buckets);
 
     return ggwResponse;
   }
