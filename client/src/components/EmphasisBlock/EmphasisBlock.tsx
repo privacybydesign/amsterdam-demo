8362eb60f8b4ba0420a94305c355a368fcd22c2e
Tom Jonkman <tom@tomjonkman.com>
2020-08-11 12:02:20 +0200

    Fix EmphasisBlock

--- client/src/components/EmphasisBlock/EmphasisBlock.tsx
+++ client/src/components/EmphasisBlock/EmphasisBlock.tsx
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
-import { themeColor, themeSpacing } from '@datapunt/asc-ui';
+import { themeColor, themeSpacing, breakpoint } from '@datapunt/asc-ui';
 
 export default styled.div`
     background-color: ${themeColor('tint', 'level2')};
-    padding-left: 220px;
-    padding-right: 220px;
-    margin: ${themeSpacing(3)} -220px;
+    margin: 0 -${themeSpacing(6)} ${themeSpacing(3)} -${themeSpacing(6)};
+    padding: ${themeSpacing(4)} ${themeSpacing(6)};
+
+    @media ${breakpoint('min-width', 'tabletM')} {
+        background-color: transparent;
+        padding: 0;
+        margin: 0;
+    }
 `;
