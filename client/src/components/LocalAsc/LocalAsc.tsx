21d2414154c7c44ecb5c2218634da8791162571e
Jasper Swart <jasper.g.swart@gmail.com>
2020-08-13 13:24:00 +0200

    added H3 and H4

--- client/src/components/LocalAsc/LocalAsc.tsx
+++ client/src/components/LocalAsc/LocalAsc.tsx
@@ -27,6 +27,16 @@ export const H2 = styled.h2`
     margin: ${themeSpacing(3, 0)};
 `;
 
+export const H3 = styled.h3`
+    line-height: ${lineHeight};
+    margin: ${themeSpacing(2, 0)};
+`;
+
+export const H4 = styled.h4`
+    line-height: ${lineHeight};
+    margin: ${themeSpacing(2, 0)};
+`;
+
 export const Paragraph = styled(AscParagraph)`
     line-height: ${lineHeight};
     margin-top: ${themeSpacing(2)};
