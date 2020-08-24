26badda41ebc735b40e73b5c1c8fa9ca06e9c432
Tom Jonkman <tom@tomjonkman.com>
2020-08-24 12:38:19 +0200

    Add Usabilla functions

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from '@app/App';
-import { initUsabilla } from '@services/usabilla';
+import '@services/usabilla';
 
 ReactDOM.render(<App />, document.getElementById('app'));
-initUsabilla();
--- client/src/services/usabilla.ts
+++ client/src/services/usabilla.ts
@@ -1,23 +1,21 @@
 import 'lightningjs/lightningjs-embed.js';
 
-let usabilla_live_feedback;
-let usabilla_live_survey;
+let usabilla_live;
 
-export const initUsabilla = async (): Promise<void> => {
-    // usabilla_live_feedback = await window['lightningjs'].require('usabilla_live', '//w.usabilla.com/1400b371716f.js');
-    // usabilla_live_survey = await window['lightningjs'].require('usabilla_live', '//w.usabilla.com/f87e3e0dea79.js');
-    // toggleUsabillaFeedback(true);
-    // toggleSurvey(false);
+const initUsabilla = (): void => {
+    usabilla_live = window['lightningjs'].require('usabilla_live', '//w.usabilla.com/1400b371716f.js');
 };
 
-/*
-export const toggleUsabillaFeedback = (flag: boolean): void => {
-    if (!usabilla_live_feedback) return;
-    // usabilla_live_feedback(flag ? 'show' : 'hide');
-    usabilla_live_survey('click');
+export const updatePageView = (): void => {
+    if (usabilla_live) {
+        usabilla_live('virtualPageView');
+    }
 };
 
-export const toggleSurvey = (flag: boolean): void => {
-    if (!usabilla_live_survey) return;
-    usabilla_live_survey(flag ? 'show' : 'hide');
-};*/
+export const startSurvey = (): void => {
+    if (usabilla_live) {
+        usabilla_live('trigger', 'survey');
+    }
+};
+
+initUsabilla();
