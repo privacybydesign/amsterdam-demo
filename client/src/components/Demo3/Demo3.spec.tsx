8ffb1b1ff4da5dd2e252800c022b53663932da55
Jasper Swart <jasper.g.swart@gmail.com>
2020-08-25 13:43:41 +0200

    added mockImplementation

--- client/src/components/Demo3/Demo3.spec.tsx
+++ client/src/components/Demo3/Demo3.spec.tsx
@@ -1,45 +1,49 @@
 import React from 'react';
 import axios from 'axios';
 import Demo3 from './Demo3';
-import { withAppContext } from './../../test/utils';
+import { withAppContext } from '../../test/utils';
+import QRCode from '@components/QRCode/QRCode';
+import createIrmaSession, { getConfig } from '@services/createIrmaSession';
 import { render, fireEvent, act, queryByAltText } from '@testing-library/react';
-import MockAdapter from 'axios-mock-adapter';
 
-const mock = new MockAdapter(axios);
+jest.mock('@services/createIrmaSession');
 
-describe.only('Demo3 component', () => {
-    it('should render correctly in demo demo and result page', async () => {
-        mock.onGet('/config').reply(200, {
-            irma: 'irma-server'
-        });
+// interface Itest {
+// fullname?: string;
+// bsn?: string;
+// }
 
-        mock.onGet('/getsession/demo3?demo=true').reply(200, {
-            sessionPtr: {
-                u: 'https://acc.attr.auth.amsterdam.nl/irma/session/IikJk1eDBZV9iMb79gR3',
-                irmaqr: 'disclosing'
-            },
-            token: '6aH2xaaLtUKgx3PknxDA'
-        });
+describe('Demo3', () => {
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
 
-        // {  bsn: '123456789',
-        // fullname: 'Willen van Zanten'
+    it('should render correctly in demo result page', async () => {
+        // const test = createIrmaSession as jest.Mock<unknown>;
+        await createIrmaSession.mockImplementation(() =>
+            Promise.resolve({
+                fullname: 'yooo',
+                bsn: '1234567'
+            })
+        );
+
+        // getConfig.mockImplementation(() => Promise.resolve({
+        //   foo: 42
+        // }));
+        // test.mockResolvedValue({
+        //   fullname: 'yooo',
+        //   bsn: '1234567'
         // });
 
-        // 'https://acc.attr.auth.amsterdam.nl/irma/session/pFqbViPJrkohP2KUR1fZ/status'
-
-        // 'https://acc.attr.auth.amsterdam.nl/session/mfeXW6zqF1gdtpF1hMeX/result'
-        // {"token":"mfeXW6zqF1gdtpF1hMeX","status":"DONE","type":"disclosing","proofStatus":"VALID","disclosed":[[{"rawvalue":"J.G. Swart","value":{"":"J.G. Swart","en":"J.G. Swart","nl":"J.G. Swart"},"id":"pbdf.gemeente.personalData.fullname","status":"PRESENT","issuancetime":1591228800},{"rawvalue":"191822668","value":{"":"191822668","en":"191822668","nl":"191822668"},"id":"pbdf.gemeente.personalData.bsn","status":"PRESENT","issuancetime":1591228800}]]}
-
-        // 'https://acc.attr.auth.amsterdam.nl/session/mfeXW6zqF1gdtpF1hMeX/result'
-        // {"token":"mfeXW6zqF1gdtpF1hMeX","status":"DONE","type":"disclosing","proofStatus":"VALID","disclosed":[[{"rawvalue":"J.G. Swart","value":{"":"J.G. Swart","en":"J.G. Swart","nl":"J.G. Swart"},"id":"pbdf.gemeente.personalData.fullname","status":"PRESENT","issuancetime":1591228800},{"rawvalue":"191822668","value":{"":"191822668","en":"191822668","nl":"191822668"},"id":"pbdf.gemeente.personalData.bsn","status":"PRESENT","issuancetime":1591228800}]]}
+        // (getConfig as jest.Mock<Itest>).mockImplementation(() => Promise.resolve(({
+        //   yo: 42
+        // })));
 
         const { container, asFragment, debug, queryAllByText, getByTestId } = render(withAppContext(<Demo3 />));
 
-        expect(queryAllByText('Demo 3: Inloggen met IRMA').length).toEqual(2);
-
-        // expect(asFragment()).toMatchSnapshot();
-
-        fireEvent.click(getByTestId('qrCodeButton'));
+        act(() => {
+            fireEvent.click(getByTestId('qrCodeButton'));
+        });
 
         act(() => {
             expect(queryAllByText('Demo 3: Inloggen met IRMA').length).toEqual(2);
