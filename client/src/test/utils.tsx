6f5aa051677b6cfd06d7808cfe467ec18f1b40c9
Tom Jonkman <tom@tomjonkman.com>
2020-08-26 22:06:33 +0200

    Enhance setup with wrapped render and mocks for all tests

--- client/src/test/utils.tsx
+++ client/src/test/utils.tsx
@@ -1,14 +1,58 @@
-import React from 'react';
-import { ThemeProvider } from '@datapunt/asc-ui';
-import { Router } from 'react-router-dom';
+import React, { ReactElement } from 'react';
+import { Route } from 'react-router';
 import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import { toBeVisible, toBeInTheDocument } from '@testing-library/jest-dom/matchers';
+import { render, RenderResult, RenderOptions } from '@testing-library/react';
+import { GlobalStyle, ThemeProvider } from '@datapunt/asc-ui';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import theme from '@services/theme';
+
+expect.extend({ toBeVisible, toBeInTheDocument });
 
 export const history = createMemoryHistory();
 
-interface IProps {}
-
-export const withAppContext: React.FC<IProps> = Component => (
-    <ThemeProvider>
-        <Router history={history}>{Component}</Router>
+const providers = ({ children }): ReactElement => (
+    <ThemeProvider overrides={theme}>
+        <GlobalStyle />
+        <Router history={history}>
+            <Route path="/">{children}</Route>
+        </Router>
     </ThemeProvider>
 );
+
+export const wrappedRender = (ui: ReactElement, options?: RenderOptions): RenderResult =>
+    render(ui, { wrapper: providers, ...options });
+
+export const setupMocks = (): void => {
+    let mockedAxios;
+
+    beforeEach(() => {
+        // Mock timers
+        jest.useFakeTimers();
+
+        // Mock axios
+        mockedAxios = new MockAdapter(axios);
+        setupMockedAPI(mockedAxios);
+    });
+
+    afterEach(() => {
+        // Unmock timers
+        jest.runOnlyPendingTimers();
+        jest.useRealTimers();
+
+        // Unmock axios
+        mockedAxios.reset();
+        mockedAxios.restore();
+
+        // Restore jest mocks
+        jest.restoreAllMocks();
+    });
+};
+
+const setupMockedAPI = (mockedAxios: MockAdapter): void => {
+    mockedAxios.onGet('/config').reply(200, {
+        environment: 'acceptance'
+    });
+};
