5820b867f6cf71a4f56cac39940f2b3f87cbfe79
Tom Jonkman <tom@tomjonkman.com>
2020-08-26 09:45:04 +0200

    Fix lint issues in test/utils

--- client/src/test/utils.tsx
+++ client/src/test/utils.tsx
@@ -5,11 +5,10 @@ import { createMemoryHistory } from 'history';
 
 export const history = createMemoryHistory();
 
-export const withAppContext = (Component) => (
-  <ThemeProvider>
-    <Router history={history}>
-      {Component}
-    </Router>
-  </ThemeProvider>
-);
+interface IProps {}
 
+export const withAppContext: React.FC<IProps> = Component => (
+    <ThemeProvider>
+        <Router history={history}>{Component}</Router>
+    </ThemeProvider>
+);
