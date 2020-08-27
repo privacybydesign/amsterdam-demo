f59f7c09c002a93e1c59ca1a54cd2d5c3ec5e775
Jasper Swart <jasper.g.swart@gmail.com>
2020-08-27 16:36:20 +0200

    added zoom and search bar to map

--- client/src/components/Map/Map.tsx
+++ client/src/components/Map/Map.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 import '../../../node_modules/leaflet/dist/leaflet.css';
-import { Map, BaseLayer } from '@datapunt/arm-core'
+
+import { Map, BaseLayer, ViewerContainer, Zoom } from '@datapunt/arm-core'
+import { Input } from '@datapunt/asc-ui';
 
 interface IProps {}
 
 const StyledMap = styled(Map)`
   height: 500px;
+  margin-bottom: 20px;
 `
 
 const MapComponent: React.FC<IProps> = () => (
-  <StyledMap fullScreen>
+
+  <StyledMap>
+    <ViewerContainer
+      bottomRight={<Zoom />}
+      topLeft={
+      <Input
+        id="some-input"
+        onChange={(e) => {console.log('change', e.target.value)}}
+      />}
+    />
     <BaseLayer />
   </StyledMap>
 );
