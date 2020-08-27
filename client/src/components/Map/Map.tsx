afd219b8c2e0e89356658d155cd12e5ce0e7e5ad
Jasper Swart <jasper.g.swart@gmail.com>
2020-08-27 17:41:22 +0200

    busy adding mapInstance to map

--- client/src/components/Map/Map.tsx
+++ client/src/components/Map/Map.tsx
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import '../../../node_modules/leaflet/dist/leaflet.css';
 
+import { useMapInstance } from '@datapunt/react-maps';
 import { Map, BaseLayer, ViewerContainer, Zoom } from '@datapunt/arm-core'
 import { Input } from '@datapunt/asc-ui';
 
-interface IProps {}
+interface IProps { }
 
 const StyledMap = styled(Map)`
   height: 500px;
   margin-bottom: 20px;
 `
 
-const MapComponent: React.FC<IProps> = () => (
+const MapComponent: React.FC<IProps> = () => {
+  const mapInstance = useMapInstance();
+  console.log('--------------', mapInstance);
 
-  <StyledMap>
-    <ViewerContainer
-      bottomRight={<Zoom />}
-      topLeft={
-      <Input
-        id="some-input"
-        onChange={(e) => {console.log('change', e.target.value)}}
-      />}
-    />
-    <BaseLayer />
-  </StyledMap>
-);
+  useEffect(() => {
+    console.log('-------------- loaded', 42);
+    // mapInstance = useMapInstance();
+  }, []);
+
+
+
+  return (
+    <StyledMap>
+      <ViewerContainer
+        bottomRight={<Zoom />}
+        topLeft={
+          <Input
+            id="some-input"
+            onChange={(e) => { console.log('change', e.target.value) }}
+          />}
+      />
+      <BaseLayer />
+    </StyledMap>
+  )
+};
 
 export default MapComponent;
