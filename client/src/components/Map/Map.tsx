c342fd7e58d330903d126b2a7201a557ac53e270
Jasper Swart <jasper.g.swart@gmail.com>
2020-08-28 12:58:51 +0200

    added autosugegst

--- client/src/components/Map/Map.tsx
+++ client/src/components/Map/Map.tsx
@@ -1,38 +1,69 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import '../../../node_modules/leaflet/dist/leaflet.css';
 
-import { useMapInstance } from '@datapunt/react-maps';
+import { useMapInstance,  } from '@datapunt/react-maps';
 import { Map, BaseLayer, ViewerContainer, Zoom } from '@datapunt/arm-core'
 import { Input } from '@datapunt/asc-ui';
+// import { getAddress } from './mapActions';
+
 
 interface IProps { }
 
 const StyledMap = styled(Map)`
   height: 500px;
   margin-bottom: 20px;
-`
+`;
 
 const MapComponent: React.FC<IProps> = () => {
-  const mapInstance = useMapInstance();
-  console.log('--------------', mapInstance);
+  const autosuggestUrl= 'https://geodata.nationaalgeoregister.nl/locatieserver/v3/suggest?fq=gemeentenaam:amsterdam&fq=type:adres&q=';
+  const [url, setUrl] = useState<string>('');
+  const [query, setQuery] = useState<string>('');
+  const [mapInstance, setMapInstance] = useState<any>({});
+  const [autosuggest, setAutosuggest] = useState([]);
 
-  useEffect(() => {
-    console.log('-------------- loaded', 42);
-    // mapInstance = useMapInstance();
-  }, []);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
 
+  const fetchAutosuggest = async (url) => {
+    const response = await axios.get(url);
+    setAutosuggest(response.data?.response?.docs);
+    return response.data;
+  };
 
+  useEffect(() =>  {
+    fetchAutosuggest(url);
+  }, [url]);
+
+  useEffect(() =>  {
+    if (mapInstance) {
+
+      // mapInstance.on('click', console.log('click'));
+    }
+    console.log('----------------', mapInstance);
+  }, [mapInstance]);
 
   return (
-    <StyledMap>
+    <StyledMap setInstance={(instance) => setMapInstance(instance)}>
       <ViewerContainer
         bottomRight={<Zoom />}
         topLeft={
           <Input
             id="some-input"
-            onChange={(e) => { console.log('change', e.target.value) }}
-          />}
+            onChange={(e) => {
+              setQuery(e.target.value);
+              setUrl(`${autosuggestUrl}${e.target.value}`);
+            }}
+          />
+        }
+        topRight={
+          <ul style={{ backgroundColor: 'white', listStyleType: 'none'}}>
+            {query.length &&  autosuggest  && autosuggest.length && autosuggest.map((address) =>
+              (<li key={address.id}>{address.weergavenaam}</li>)
+          )}
+        </ul>
+    }
       />
       <BaseLayer />
     </StyledMap>
