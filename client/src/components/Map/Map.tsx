c6eba44b4405fe5c5940f151029cf48cba1952af
Jasper Swart <jasper.g.swart@gmail.com>
2020-09-01 16:38:51 +0200

    added flyTo location in map

--- client/src/components/Map/Map.tsx
+++ client/src/components/Map/Map.tsx
@@ -3,8 +3,10 @@ import styled from 'styled-components';
 import axios from 'axios';
 import '../../../node_modules/leaflet/dist/leaflet.css';
 
+import { useMapInstance,  } from '@datapunt/react-maps';
 import { Map, BaseLayer, ViewerContainer, Zoom, useGetAddressFromLatLng, Marker } from '@datapunt/arm-core'
 import { Input } from '@datapunt/asc-ui';
+import { map } from 'leaflet';
 
 interface IProps { }
 
@@ -18,8 +20,9 @@ const StyledMap = styled(Map)`
 const MapComponent: React.FC<IProps> = () => {
   // const { addresses, setLatLng, loading, latLng } = useGetAddressFromLatLng()
   const addressRef = useRef(null);
-  const autosuggestUrl= 'https://geodata.nationaalgeoregister.nl/locatieserver/v3/suggest?fq=gemeentenaam:amsterdam&fq=type:adres&q=';
+  const autosuggestUrl= 'https://geodata.nationaalgeoregister.nl/locatieserver/v3/suggest?fq=gemeentenaam:amsterdam&fq=type:adres&fl=id,weergavenaam,type,score,lat,lon&q=';
   const locationUrl = 'https://geodata.nationaalgeoregister.nl/locatieserver/revgeo?type=adres&rows=1&fl=id,weergavenaam,straatnaam,huis_nlt,postcode,woonplaatsnaam,centroide_ll&distance=50&';
+  const [mapInstance, setMapInstance] = useState<any>({});
   const [url, setUrl] = useState<string>('');
   const [query, setQuery] = useState<string>('');
   const [autosuggest, setAutosuggest] = useState([]);
@@ -43,11 +46,21 @@ const MapComponent: React.FC<IProps> = () => {
     fetchLocation(e.latlng);
   }
 
-  const onAutosuggestClick = (e: any, address: string) => {
+  const onAutosuggestClick = async (e: any, address: any) => {
     e.preventDefault();
-    if (address) {
-      addressRef.current.value = address;
+    if (address.weergavenaam) {
+      addressRef.current.value = address.weergavenaam;
       setShowAutosuggest(false);
+    }
+
+    const response = await axios.get(`https://geodata.nationaalgeoregister.nl/locatieserver/v3/lookup?id=${address.id}`)
+    if (mapInstance && response.data.response.docs[0]) {
+      const loc = response.data.response.docs[0].centroide_ll.replace(/POINT\(|\)/, '').split(' ');
+      const flyTo = { lat: parseFloat(loc[1]), lng: parseFloat(loc[0])};
+      mapInstance.flyTo(flyTo, 11);
+      // todo fix typying
+      // todo fix styling
+      // setLatLng(flyTo);
     }
   }
 
@@ -65,7 +78,7 @@ const MapComponent: React.FC<IProps> = () => {
 
   return (
     <div style={{ position: 'relative', width: '100%', height: '500px'}}>
-      <StyledMap
+      <StyledMap setInstance={(instance) => setMapInstance(instance)}
         events={{
           click: (e) => {
             onMapclick(e);
@@ -89,7 +102,7 @@ const MapComponent: React.FC<IProps> = () => {
               {showAutosuggest &&
                 <ul style={{ width: '500px', backgroundColor: 'white', listStyleType: 'none', padding: '0 0 0 12px'}}>
                   {query.length &&  autosuggest  && autosuggest.length ? autosuggest.map((address) =>
-                    (<li key={address.id}><a href="#" onClick={(e) => onAutosuggestClick(e, address.weergavenaam)}>{address.weergavenaam}</a></li>)
+                    (<li key={address.id}><a href="#" onClick={(e) => onAutosuggestClick(e, address)}>{address.weergavenaam}</a></li>)
                   ) : null}
                 </ul>
               }
