96a376e76f24f90325f08721ecbe4fb8ec59872a
Tom Jonkman <tom@tomjonkman.com>
2020-09-29 10:43:57 +0200

    Try and fix root security

--- server/node/index.js
+++ server/node/index.js
@@ -169,6 +169,10 @@ const init = async () => {
       app.get("*", secured, function (req, res) {
         res.sendFile(path.join(__dirname, config.docroot, "index.html"));
       });
+
+      app.get("/", secured, function (req, res) {
+        res.sendFile(path.join(__dirname, config.docroot, "index.html"));
+      });
     } else {
       console.log("Using proxy to the react app for development");
       // proxy the root to the react app container in development mode
