263d802e94d7a9111307ea1b9fd73045b38efa1b
Tom Jonkman <tom@tomjonkman.com>
2020-11-09 11:44:53 +0100

    Fix grey background on emphasis block

--- client/src/components/EmphasisBlock/EmphasisBlock.tsx
+++ client/src/components/EmphasisBlock/EmphasisBlock.tsx
@@ -3,8 +3,8 @@ import { themeColor, themeSpacing, breakpoint } from '@amsterdam/asc-ui';
 
 export default styled.div`
     background-color: ${themeColor('tint', 'level2')};
-    margin: 0 -220px ${themeSpacing(3)} -220px;
-    padding: ${themeSpacing(4)} 220px;
+    margin: 0 -240px ${themeSpacing(3)} -220px;
+    padding: ${themeSpacing(4)} 240px ${themeSpacing(4)} 220px;
 
     /* // TODO: Check if this grey background needs to be shown on big screens */
     /* @media ${breakpoint('min-width', 'tabletM')} {
