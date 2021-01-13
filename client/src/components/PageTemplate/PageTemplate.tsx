a82593aba0c5ef2f468ea06ea16c9a27c1104356
Frank Filius <f.filius@amsterdam.nl>
2021-01-13 16:18:18 +0100

    Add ie11 warning to template-component

--- client/src/components/PageTemplate/PageTemplate.tsx
+++ client/src/components/PageTemplate/PageTemplate.tsx
@@ -3,16 +3,38 @@ import styled from 'styled-components';
 import { Row, themeSpacing, themeColor } from '@amsterdam/asc-ui';
 import AppRoutes from '@app/AppRoutes';
 import * as AscLocal from '@components/LocalAsc/LocalAsc';
+import { Alert as AlertIcon } from '@amsterdam/asc-assets';
 import Footer from '@components/Footer/Footer';
+import content from '@services/content';
 
 interface IProps {
     className?: string;
 }
 
+declare global {
+    interface Document {
+        documentMode?: any;
+    }
+}
+
+const isIE = /*@cc_on!@*/ false || !!document.documentMode;
+
 const PageTemplate: React.FC<IProps> = ({ children, className }) => (
     <StyledRow className={className}>
         <AscLocal.Header fullWidth={false} tall homeLink={AppRoutes.HOMEPAGE.path} />
-        <StyledColumn span={12}>{children}</StyledColumn>
+        <StyledColumn span={12}>
+            {isIE && (
+                <AscLocal.Alert
+                    color={AscLocal.AlertColor.ERROR}
+                    icon={<AlertIcon />}
+                    iconSize={22}
+                    heading={content.ieErrorAlert.heading}
+                    content={content.ieErrorAlert.content}
+                    dataTestId="hasErrorAlert"
+                />
+            )}
+            {children}
+        </StyledColumn>
         <Footer />
     </StyledRow>
 );
