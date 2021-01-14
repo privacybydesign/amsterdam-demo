d16961ed44aa360ce6a48950406715ab0a516b5e
Frank Filius <f.filius@amsterdam.nl>
2021-01-14 12:39:29 +0100

    Add a bit more space to the bottom of the content-section

--- client/src/components/PageTemplate/PageTemplate.tsx
+++ client/src/components/PageTemplate/PageTemplate.tsx
@@ -44,7 +44,7 @@ const PageTemplate: React.FC<IProps> = ({ children, className }) => {
 };
 
 const StyledColumn = styled(AscLocal.Column)`
-    margin: ${themeSpacing(5)} 0;
+    margin: ${themeSpacing(5)} 0 ${themeSpacing(10)} 0;
     max-width: 940px;
 `;
 
