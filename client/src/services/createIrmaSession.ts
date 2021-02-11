e96849977e7a8bdd34b937cd01ac2a5250dc1f3e
Frank Filius <f.filius@amsterdam.nl>
2021-02-11 09:40:46 +0100

    Cleanup

--- client/src/services/createIrmaSession.ts
+++ client/src/services/createIrmaSession.ts
@@ -1,28 +1,7 @@
 import axios from 'axios';
-import * as irma from '@privacybydesign/irmajs';
 import IrmaCore from '@privacybydesign/irma-core';
 import Web from '@privacybydesign/irma-web';
 import Client from '@privacybydesign/irma-client';
-
-const nativeDrawImage = window.CanvasRenderingContext2D.prototype.drawImage;
-
-const wrapDrawImage = (holderElementId: string) => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    window.CanvasRenderingContext2D.prototype.drawImage = function (...args: any) {
-        // Draw the original image
-        nativeDrawImage.call(this, ...args);
-
-        // Remove the old logo
-        const canvas = document.getElementById(holderElementId) as HTMLCanvasElement;
-        const ctx = canvas.getContext('2d');
-        ctx.fillStyle = '#dddbdb';
-        ctx.fillRect(82, 62, 65, 100);
-    };
-};
-
-const unwrapDrawImage = () => {
-    window.CanvasRenderingContext2D.prototype.drawImage = nativeDrawImage;
-};
 
 // Types
 export interface IIrmaServerConfig {
@@ -50,51 +29,7 @@ export const isMobile = (): boolean => {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 };
 
-// Note: To use the demo credentials on non-production environments add ?demo=true to the URL
 const createIrmaSession = async (dataType: string, holderElementId: string, query = {}): Promise<unknown> => {
-    const config = await getConfig();
-
-    const queryString = Object.keys(query)
-        .map((key, index) => `${index === 0 ? '?' : ''}${key}=${query[key]}`)
-        .join('&');
-
-    const irmaResponse = await axios.get(`/getsession/${dataType}${queryString}`);
-    const session = await irmaResponse.data;
-
-    const { sessionPtr, token } = session;
-
-    const sessionOptions = {
-        method: 'canvas',
-        element: holderElementId,
-        showConnectedIcon: true,
-        server: config.irma,
-        token,
-        language: 'nl',
-        disableMobile: true
-    };
-
-    if (isMobile()) {
-        sessionOptions.method = 'mobile';
-        sessionOptions.disableMobile = false;
-    }
-
-    try {
-        // This function wraps the canvas context drawImage method to be able to run some code when the QR code disappears
-        wrapDrawImage(holderElementId);
-        // Adjust malformed url returned from irma server due to rewrite
-        sessionPtr.u = sessionPtr.u.replace(/\/irma/g, '/irma/irma');
-        const result = await irma.handleSession(sessionPtr, sessionOptions);
-        console.log('result: ', result);
-        unwrapDrawImage();
-
-        // Only get the last part of each result
-        return reduceIRMAResult(result.disclosed);
-    } catch (e) {
-        return null;
-    }
-};
-
-const createIrmaSession2 = async (dataType: string, holderElementId: string, query = {}): Promise<unknown> => {
     const queryString = Object.keys(query)
         .map((key, index) => `${index === 0 ? '?' : ''}${key}=${query[key]}`)
         .join('&');
@@ -157,4 +92,4 @@ const reduceIRMAResult = (disclosedCredentialSets: IDisclosedCredentialSet[]) =>
     return joinedResults;
 };
 
-export default createIrmaSession2;
+export default createIrmaSession;
