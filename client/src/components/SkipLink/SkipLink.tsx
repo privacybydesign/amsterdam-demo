d6343c524744e2e5c35d875105cf13fc63e32f19
Frank Filius <f.filius@amsterdam.nl>
2021-02-25 13:53:00 +0100

    fix: remove unused import

--- client/src/components/SkipLink/SkipLink.tsx
+++ client/src/components/SkipLink/SkipLink.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Button, themeColor, themeSpacing } from '@amsterdam/asc-ui';
+import { Button, themeSpacing } from '@amsterdam/asc-ui';
 import content from '@services/content';
 
 interface IProps {
