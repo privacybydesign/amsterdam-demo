c45abfdb859247989fb697267a8be8273c0f75b9
Frank Filius <f.filius@amsterdam.nl>
2021-03-03 10:40:12 +0100

    feat: create page

---
+++ client/src/components/IrmaVote/IrmaVote.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import irma from '@privacybydesign/irma-frontend';
+
+const IrmaVote: React.FC = () => {
+    return (
+        <div className="irma-vote">
+            <h1>Stemmen met IRMA</h1>
+        </div>
+    );
+};
+
+export default IrmaVote;
