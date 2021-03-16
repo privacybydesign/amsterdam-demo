eac97790200075e4da7e704d07a83f98772c397c
Tom Jonkman <tom@tomjonkman.com>
2021-03-16 11:03:47 +0100

    Log process.cwd

--- server/src/loaders/express.ts
+++ server/src/loaders/express.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 
 // Setup Express
 export default ({ app }: { app: express.Application }) => {
     // Health check endpoint
     app.get('/health', (req, res) => res.status(200).send('Healthy!'));
+    app.get('/cwd', (req, res) => res.status(200).send(`${path.join(process.cwd(), 'static/403.html')}`));
 
     // Enable Cross Origin Resource Sharing to all origins by default
     app.use(cors());
