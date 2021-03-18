2c84dd03c063e6bcfcb00ec7eed73a3f26ec89f9
Tom Jonkman <tom@tomjonkman.com>
2021-03-18 14:50:24 +0100

    Add 400 result for missing session token

--- server/src/api/result.ts
+++ server/src/api/result.ts
@@ -7,15 +7,19 @@ import Logger from '@loaders/logger';
 // Define routes for demo
 export default (router: Router) => {
     router.get('/result', cors(), async (req: Request, res: Response) => {
-        Logger.info(`Incoming request for session result`);
+        Logger.info(`Incoming request for session result for session ${req.session!.token}`);
         const irmaServiceInstance = Container.get(IrmaService);
-        const result = await irmaServiceInstance.requestSessionResult(req.session!.token);
+        if (req.session && req.session.token) {
+            const result = await irmaServiceInstance.requestSessionResult(req.session!.token);
 
-        // Destroy session when session is done
-        if (result && result.status === 'DONE') {
-            (req.session!.destroy as any)();
+            // Destroy session when session is done
+            if (result && result.status === 'DONE') {
+                (req.session!.destroy as any)();
+            }
+
+            return res.status(200).json(result);
+        } else {
+            return res.status(400).send('No session was found for this request.');
         }
-
-        return res.status(200).json(result);
     });
 };
