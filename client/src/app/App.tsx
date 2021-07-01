e36c2261faa318be35376dedeade35b3d3cb2458
Tom Jonkman <tom@tomjonkman.com>
2021-07-01 12:23:02 +0200

    Bump dependencies

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch, useLocation } from 'react-router-dom';
 import { GlobalStyle, ThemeProvider, themeColor } from '@amsterdam/asc-ui';
 import theme from '@services/theme';
 import AppRoutes from './AppRoutes';
@@ -8,22 +8,29 @@ import ScrollToTop from '@components/ScrollToTop/ScrollToTop';
 
 interface IProps {}
 
-const App: React.FC<IProps> = () => (
-    <ThemeProvider overrides={theme}>
-        <Bg>
-            <GlobalStyle />
-            <Router>
-                <ScrollToTop />
-                <Switch>
-                    {Object.keys(AppRoutes).map(key => {
-                        const { path, component, exact } = AppRoutes[key];
-                        return <Route key={key} path={path} component={component} exact={exact} />;
-                    })}
-                </Switch>
-            </Router>
-        </Bg>
-    </ThemeProvider>
-);
+const App: React.FC<IProps> = () => {
+    const location = useLocation();
+    useEffect(() => {
+        console.log(location);
+    }, [location]);
+
+    return (
+        <ThemeProvider overrides={theme}>
+            <Bg>
+                <GlobalStyle />
+                <Router>
+                    <ScrollToTop />
+                    <Switch>
+                        {Object.keys(AppRoutes).map(key => {
+                            const { path, component, exact } = AppRoutes[key];
+                            return <Route key={key} path={path} component={component} exact={exact} />;
+                        })}
+                    </Switch>
+                </Router>
+            </Bg>
+        </ThemeProvider>
+    );
+};
 
 const Bg = styled.div`
     background-color: ${themeColor('tint', 'level3')};
