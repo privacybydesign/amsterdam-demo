fbfa856d7a091260ec2c8f1db534a6c349825d93
Mike Alders <mike.alders@amsterdam.nl>
2022-04-19 18:15:10 +0200

    Update credential by IRMA
    See update in Slack: https://irmacard.slack.com/archives/C0KCTQ0BC/p1631523627008400

--- server/src/api/demo5.ts
+++ server/src/api/demo5.ts
@@ -20,7 +20,7 @@ const demoCredentials: IDemoCredentials = {
     PRODUCTION: ({ mobileNumber, email }: { mobileNumber: boolean; email: boolean }) => {
         const credentials = [];
         if (mobileNumber === true) {
-            credentials.push([['pbdf.pbdf.mobilenumber.mobilenumber']]);
+            credentials.push([['pbdf.pbdf.mobilenumber.mobilenumber'], ['pbdf.sidn-pbdf.mobilenumber.mobilenumber']]);
         }
 
         if (email === true) {
