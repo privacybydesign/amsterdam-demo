5d6d2a8f77df3aa3d160f4871eb800361ded98a5
Mike Alders <mike.alders@amsterdam.nl>
2023-03-03 11:34:47 +0100

    Remove old PbdF credential
    No longer use of old e-mail credential allowed

--- server/src/api/demo3.ts
+++ server/src/api/demo3.ts
@@ -13,7 +13,7 @@ const demoCredentials: IDemoCredentials = {
             ['pbdf.pilot-amsterdam.idcard.firstnames', 'pbdf.pilot-amsterdam.idcard.surname'],
             ['pbdf.gemeente.personalData.fullname']
         ],
-        [['pbdf.pbdf.email.email'], ['pbdf.sidn-pbdf.email.email']]
+        ['pbdf.sidn-pbdf.email.email']
     ]
 };
 
