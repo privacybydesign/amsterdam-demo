14779c413f47aa0270a7606aaae3dcf4d396ded9
Mike Alders <mike.alders@amsterdam.nl>
2023-03-07 14:30:26 +0100

    Redirect to why Yivi
    Redirect to explanation on Innovatie

--- server/src/api/redirects.ts
+++ server/src/api/redirects.ts
@@ -6,4 +6,9 @@ export default (router: Router) => {
     router.get('/zonder-gedoe-op-pad', (req, res: Response) => {
         res.status(301).redirect('https://www.notion.so/Zonder-gedoe-op-pad-90d725fc2e2745e6bee322b76985125b');
     });
+    // Waarom Yivi?
+    router.get('/inloggen-met-yivi', (req, res: Response) => {
+        res.status(301).redirect('https://www.amsterdam.nl/innovatie/digitalisering-technologie/digitalisering/irma-nieuwe-manier-inloggen');
+    });
+
 };
